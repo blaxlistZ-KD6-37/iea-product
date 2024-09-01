54dd7c08e70055a1ad32d4d5c03f85a0881bbd3a
blaxlistZ <blaxlistz@gmail.com>
2024-09-01 22:17:00 +0800

    "Modified handler to be an array."

--- src/go/select_handler.go
+++ src/go/select_handler.go
@@ -15,16 +15,32 @@ func (dat *db_handler_st) handlerProductName(w http.ResponseWriter, r *http.Requ
 	})
 }
 
-func (dat *db_handler_st) SelectionDatabase(col, table string) string {
-	var element string
+func (dat *db_handler_st) SelectionDatabase(col, table string) []string {
+	var elements []string
 	formatted_value := fmt.Sprintf("SELECT %s FROM %s", col, table)
 
-	err := dat.DB.QueryRow(formatted_value).Scan(&element)
+	rows, err := dat.DB.Query(formatted_value)
 	if err != nil {
 		log.Printf("Error in SelectionDatabase: %v", err)
+		return elements
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var element string
+		err := rows.Scan(&element)
+		if err != nil {
+			log.Printf("Error scanning row in SelectionDatabase: %v", err)
+			continue
+		}
+		elements = append(elements, element)
 	}
 
-	return element
+	if err = rows.Err(); err != nil {
+		log.Printf("Error iterating rows in SelectionDatabase: %v", err)
+	}
+
+	return elements
 }
 
 func newHandler(dat *sql.DB) *db_handler_st {
