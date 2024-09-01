6c54107357d9183a3477954ae7a4692481e708cf
blaxlistZ <blaxlistz@gmail.com>
2024-09-01 22:17:37 +0800

    "Other updates."

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -27,23 +27,3 @@ pivot_vec.addEventListener("click", () => {
     frame_parent.classList.toggle("-z-50");
   });
 });
-
-const price_val = <HTMLDivElement>document.querySelector(".price-val");
-
-async function fetchPrice(): Promise<void> {
-  try {
-    const response = await fetch(
-      "http://localhost:8080/A1_/runlive/product/name"
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    price_val.innerHTML = `${data.name}`;
-  } catch (error) {
-    console.error("Error fetching price:", error);
-    price_val.innerHTML = "Error loading price";
-  }
-}
-
-fetchPrice();
