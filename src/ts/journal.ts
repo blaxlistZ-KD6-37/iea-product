0c13abc1136826de57d8fc68574c5439f6a67306
blaxlistZ <blaxlistz@gmail.com>
2024-09-02 22:34:34 +0800

    "Fetch and tested database for transaction."

--- src/ts/journal.ts
+++ src/ts/journal.ts
@@ -1,19 +1,24 @@
-    // const price_val = <HTMLDivElement>document.querySelector(".price-val");
+const price_val = <HTMLDivElement>document.querySelector(".price-val");
 
-    // async function fetchPrice(): Promise<void> {
-    //   try {
-    //     const response = await fetch(
-    //       "http://localhost:8080/A1_/runlive/product/name"
-    //     );
-    //     if (!response.ok) {
-    //       throw new Error(`HTTP error! status: ${response.status}`);
-    //     }
-    //     const data = await response.json();
-    //     price_val.innerHTML = `${data.name}`;
-    //   } catch (error) {
-    //     console.error("Error fetching price:", error);
-    //     price_val.innerHTML = "Error loading price";
-    //   }
-    // }
+async function fetchTransaction(): Promise<void> {
+  try {
+    const response = await fetch(
+      "http://localhost:8080/A1_/runlive/transaction_detail"
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const k = 5;
+    const data = await response.json();
+    price_val.innerHTML = ` (Key: ${data.transaction_detail_id[k]}),
+                                        (Transaction ID: ${data.transaction_id_fk[k]}),
+                                        (Account ID: ${data.account_id_fk[k]}),
+                                        (Amount: ${data.amount[k]}),
+                                        (Debit: ${data.is_debit[k]})`;
+  } catch (error) {
+    console.error("Error fetching price:", error);
+    price_val.innerHTML = "Error loading price";
+  }
+}
 
-    // fetchPrice();
+fetchTransaction();
