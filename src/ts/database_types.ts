5aa21aa980fa51dee97f64c90aa5e3c45aedd9b8
blaxlistZ <blaxlistz@gmail.com>
2024-09-05 10:00:43 +0800

    "Refactored database objects."

--- src/ts/database_types.ts
+++ src/ts/database_types.ts
@@ -1,13 +1,11 @@
 export interface Database_Types {
-  account_id: string[];
-  account_name: string[];
-  account_category: string[];
-  transaction_id: string[];
-  transaction_date: string[];
-  transaction_description: string[];
-  transaction_detail_id: string[];
-  account_id_fk: string[];
-  transaction_id_fk: string[];
-  transaction_amount: string[];
-  transaction_is_debit: string[];
+  account_id: number;
+  name: string;
+  category: string;
+  transaction_tab_id: number;
+  date: Date;
+  description: string;
+  transaction_detail_id: number;
+  amount: number;
+  is_debit: boolean;
 }
