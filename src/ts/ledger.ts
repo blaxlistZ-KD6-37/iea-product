f7c80be556703342a8e270c4225ec08fbc42dd5f
blaxlistZ <blaxlistz@gmail.com>
2024-09-20 22:56:03 +0800

    "Added more things in ledger functionalities."

--- src/ts/ledger.ts
+++ src/ts/ledger.ts
@@ -45,12 +45,35 @@ account_container.appendChild(account_name);
 const account_type_wrapper = <HTMLDivElement>document.createElement("div");
 
 const debit_doc = <HTMLDivElement>document.createElement("div");
+debit_doc.classList.add("side-credit");
 // Debit Datas
 const debit_data = <HTMLDivElement>document.createElement("div");
+debit_data.classList.add("data");
 // Debits
 const cash_debit = financial_accounting_ob.filter((acc) => {
-  return acc.name === unique_acc[0] && acc.is_debit === true;
+  acc.category === "Asset" &&
+    acc.name === unique_acc[0] &&
+    acc.is_debit === false &&
+    new Date(acc.date).toString() === new Date("2024-09-05").toString();
 });
+let debit_summation = 0;
+cash_debit.forEach((cash) => {
+  debit_summation += cash.amount;
+});
+
+const debit_data_account = <HTMLDivElement>document.createElement("div");
+debit_data_account.classList.add("d-acc_wrapper");
+const debit_data_date = <HTMLTimeElement>document.createElement("time");
+debit_data_date.classList.add("date");
+const debit_formatted_date = ``;
+debit_data_date.textContent = debit_formatted_date;
+debit_data_date.dateTime = ``;
+debit_data_account.appendChild(debit_data_date);
+
+const debit_data_amount = <HTMLDivElement>document.createElement("div");
+debit_data_amount.classList.add("date-acc-amount");
+debit_data_amount.innerHTML = `&#x20B1;${debit_summation.toFixed(2)}`;
+debit_data_account.appendChild(debit_data_amount);
 
 const debit_total = <HTMLDivElement>document.createElement("div");
 
@@ -77,10 +100,10 @@ const credit_data_account = <HTMLDivElement>document.createElement("div");
 credit_data_account.classList.add("d-acc_wrapper");
 const credit_data_date = <HTMLTimeElement>document.createElement("time");
 credit_data_date.classList.add("date");
-const formatted_date = `(${new Date(cash_credit[0].date)
+const credit_formatted_date = `(${new Date(cash_credit[0].date)
   .toISOString()
   .slice(5, 7)}/${new Date(cash_credit[0].date).toUTCString().slice(5, 7)})`;
-credit_data_date.textContent = formatted_date;
+credit_data_date.textContent = credit_formatted_date;
 credit_data_date.dateTime = new Date(cash_credit[0].date).toISOString();
 credit_data_account.appendChild(credit_data_date);
 
@@ -90,6 +113,9 @@ credit_data_amount.innerHTML = `&#x20B1;${credit_summation.toFixed(2)}`;
 credit_data_account.appendChild(credit_data_amount);
 
 const credit_total = <HTMLDivElement>document.createElement("div");
+
+console.log(credit_data_account);
+console.log(debit_data_account);
 
 // Balance
 const balance = <HTMLDivElement>document.createElement("div");
