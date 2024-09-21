91232c8aa7a7d0114a7e86a57ffe4f4d07b6808c
blaxlistZ <blaxlistz@gmail.com>
2024-09-21 20:56:08 +0800

    "Finished main functionalities for ledger."

--- src/ts/ledger.ts
+++ src/ts/ledger.ts
@@ -11,26 +11,6 @@ const financial_accounting_ob = await api.GetDB<financial_accounting_t[]>(
   "financial_account"
 );
 
-type account_sig =
-  | "debit_entry"
-  | "debit_value_entry"
-  | "credit_entry"
-  | "credit_value_entry";
-
-type account_entries = Record<account_sig, HTMLDivElement>;
-
-// Account Container
-const account_container_main = <NodeListOf<HTMLDivElement>>(
-  document.querySelectorAll(".acc-container_wrapper")
-);
-
-const account_container = <HTMLDivElement>document.createElement("div");
-account_container.classList.add("acc-container");
-
-// Account Name
-const account_name = <HTMLDivElement>document.createElement("div");
-account_name.classList.add("acc-name");
-
 // Filtering Unique Names
 const filterUniqueByCategory = (category_type: string): string[] => {
   const category_acc: financial_accounting_t[] = financial_accounting_ob.filter(
@@ -46,27 +26,32 @@ const filterUniqueByCategory = (category_type: string): string[] => {
   return unique_list;
 };
 
-const account_debit_name = <HTMLDivElement>document.createElement("div");
-account_debit_name.classList.add("name-debit");
-account_debit_name.textContent = `DR`;
-account_name.appendChild(account_debit_name);
+// Account Name
+const createNameElement = (current_account: string): HTMLDivElement => {
+  const account_name = <HTMLDivElement>document.createElement("div");
+  account_name.classList.add("acc-name");
 
-const unique_acc: string[] = filterUniqueByCategory("Asset");
-const account_title = <HTMLDivElement>document.createElement("div");
-account_title.classList.add("name-title");
-account_title.textContent = unique_acc[0];
-account_name.appendChild(account_title);
+  const account_debit_name = <HTMLDivElement>document.createElement("div");
+  account_debit_name.classList.add("name-debit");
+  account_debit_name.textContent = `DR`;
+  account_name.appendChild(account_debit_name);
 
-const account_credit_name = <HTMLDivElement>document.createElement("div");
-account_credit_name.classList.add("name-credit");
-account_credit_name.textContent = `CR`;
-account_name.appendChild(account_credit_name);
+  const account_title = <HTMLDivElement>document.createElement("div");
+  account_title.classList.add("name-title");
+  account_title.textContent = current_account;
+  account_name.appendChild(account_title);
+
+  const account_credit_name = <HTMLDivElement>document.createElement("div");
+  account_credit_name.classList.add("name-credit");
+  account_credit_name.textContent = `CR`;
+  account_name.appendChild(account_credit_name);
+
+  return account_name;
+};
 
 // Debit-Credit
-type data_acc_t = Pick<
-  Database_Objects,
-  "name" | "category" | "date" | "is_debit"
->;
+type data_acc_t = Pick<Database_Objects, "name" | "category" | "is_debit"> &
+  Partial<Pick<Database_Objects, "date">>;
 
 const sumForDate = (items: number[]): number => {
   let summation: number = 0;
@@ -75,20 +60,6 @@ const sumForDate = (items: number[]): number => {
   });
 
   return summation;
-};
-
-const debit_obj: data_acc_t = {
-  name: unique_acc[0],
-  category: "Asset",
-  date: new Date("2024-09-19"),
-  is_debit: true,
-};
-
-const credit_obj: data_acc_t = {
-  name: unique_acc[0],
-  category: "Asset",
-  date: new Date("2024-09-19"),
-  is_debit: false,
 };
 
 const createAccountData = (data_account: data_acc_t): HTMLDivElement => {
@@ -102,7 +73,8 @@ const createAccountData = (data_account: data_acc_t): HTMLDivElement => {
       acc.category === data_account.category &&
       acc.name === data_account.name &&
       acc.is_debit === data_account.is_debit &&
-      new Date(acc.date).toString() === new Date(data_account.date).toString()
+      new Date(acc.date).toString() ===
+        new Date(<Date>data_account.date).toString()
     );
   });
 
@@ -152,6 +124,7 @@ const createMainData = (target: data_acc_t): HTMLDivElement => {
       date: date,
       is_debit: target.is_debit,
     };
+
     const dat_acc: HTMLDivElement = createAccountData(dat_obj);
     data.appendChild(dat_acc);
   });
@@ -179,29 +152,33 @@ const createDataSum = (target: data_acc_t): HTMLDivElement => {
   return data_total;
 };
 
-const account_type_wrapper = <HTMLDivElement>document.createElement("div");
-account_type_wrapper.classList.add("side-wrapper");
+const createSideWrapperElement = (
+  debit_cond: data_acc_t,
+  credit_cond: data_acc_t
+): HTMLDivElement => {
+  const account_type_wrapper = <HTMLDivElement>document.createElement("div");
+  account_type_wrapper.classList.add("side-wrapper");
 
-const debit_doc = <HTMLDivElement>document.createElement("div");
-debit_doc.classList.add("side-debit");
-const debit_data = createMainData(debit_obj);
-const debit_total: HTMLDivElement = createDataSum(debit_obj);
-debit_doc.appendChild(debit_data);
-debit_doc.appendChild(debit_total);
-account_type_wrapper.appendChild(debit_doc);
+  const debit_doc = <HTMLDivElement>document.createElement("div");
+  debit_doc.classList.add("side-debit");
+  const debit_data = createMainData(debit_cond);
+  const debit_total: HTMLDivElement = createDataSum(debit_cond);
+  debit_doc.appendChild(debit_data);
+  debit_doc.appendChild(debit_total);
+  account_type_wrapper.appendChild(debit_doc);
 
-const credit_doc = <HTMLDivElement>document.createElement("div");
-credit_doc.classList.add("side-credit");
-const credit_data = createMainData(credit_obj);
-const credit_total: HTMLDivElement = createDataSum(credit_obj);
-credit_doc.appendChild(credit_data);
-credit_doc.appendChild(credit_total);
-account_type_wrapper.appendChild(credit_doc);
+  const credit_doc = <HTMLDivElement>document.createElement("div");
+  credit_doc.classList.add("side-credit");
+  const credit_data = createMainData(credit_cond);
+  const credit_total: HTMLDivElement = createDataSum(credit_cond);
+  credit_doc.appendChild(credit_data);
+  credit_doc.appendChild(credit_total);
+  account_type_wrapper.appendChild(credit_doc);
+
+  return account_type_wrapper;
+};
 
 // Balance
-const balance = <HTMLDivElement>document.createElement("div");
-balance.classList.add("balance");
-
 const stringDocToNumeric = (item: HTMLDivElement): number => {
   let string_item = <string>item.textContent;
   string_item = <string>string_item.slice(1);
@@ -210,26 +187,121 @@ const stringDocToNumeric = (item: HTMLDivElement): number => {
 
   return numeric_item;
 };
-const balance_amount = <HTMLDivElement>document.createElement("div");
-balance_amount.classList.add("balance-amount");
-const numeric_debit: number = stringDocToNumeric(debit_total);
-const numeric_credit: number = stringDocToNumeric(credit_total);
-const account_difference: number = numeric_debit - numeric_credit;
-balance_amount.innerHTML = `&#x20B1;${account_difference.toFixed(2)}`;
-if (account_difference < 0) {
-  balance_amount.innerHTML = `(&#x20B1;${Math.abs(account_difference).toFixed(
-    2
-  )})`;
-}
-balance.appendChild(balance_amount);
 
-const bal_space = <HTMLDivElement>document.createElement("div");
-bal_space.classList.add("balance-space");
-balance.appendChild(bal_space);
+const createBalanceElement = (
+  account_type_wrapper: HTMLDivElement,
+  category_type: string
+): HTMLDivElement => {
+  const balance = <HTMLDivElement>document.createElement("div");
+  balance.classList.add("balance");
+  const debit_side = <HTMLDivElement>(
+    (<HTMLDivElement>account_type_wrapper.firstChild).lastChild
+  );
+  const credit_side = <HTMLDivElement>(
+    (<HTMLDivElement>account_type_wrapper.lastChild).lastChild
+  );
 
-account_container.appendChild(account_name);
-account_container.appendChild(account_type_wrapper);
-account_container.appendChild(balance);
+  const balance_amount = <HTMLDivElement>document.createElement("div");
+  balance_amount.classList.add("balance-amount");
+  const numeric_debit: number = stringDocToNumeric(debit_side);
+  const numeric_credit: number = stringDocToNumeric(credit_side);
+  const account_difference: number =
+    category_type !== "Asset"
+      ? numeric_credit - numeric_debit
+      : numeric_debit - numeric_credit;
+  balance_amount.innerHTML = `&#x20B1;${account_difference.toFixed(2)}`;
 
-account_container_main[0].appendChild(account_container);
-console.log(account_container_main[0]);
+  if (account_difference < 0) {
+    balance_amount.innerHTML = `(&#x20B1;${Math.abs(account_difference).toFixed(
+      2
+    )})`;
+  }
+
+  balance.appendChild(balance_amount);
+
+  const bal_space = <HTMLDivElement>document.createElement("div");
+  bal_space.classList.add("balance-space");
+  balance.appendChild(bal_space);
+  return balance;
+};
+
+// Account Container
+const account_container_main = <NodeListOf<HTMLDivElement>>(
+  document.querySelectorAll(".acc-container_wrapper")
+);
+
+const createContainerElement = (
+  debit_cond: data_acc_t,
+  credit_cond: data_acc_t
+): HTMLDivElement => {
+  if (
+    debit_cond.name !== credit_cond.name ||
+    debit_cond.category !== credit_cond.category ||
+    debit_cond.is_debit === credit_cond.is_debit
+  ) {
+    throw new Error("Something is wrong with the debit and credit conditions.");
+  }
+
+  const account_container = <HTMLDivElement>document.createElement("div");
+  account_container.classList.add("acc-container");
+
+  const account_name: HTMLDivElement = createNameElement(debit_cond.name);
+  account_container.appendChild(account_name);
+
+  const account_type_wrapper: HTMLDivElement = createSideWrapperElement(
+    debit_cond,
+    credit_cond
+  );
+  account_container.appendChild(account_type_wrapper);
+
+  const balance: HTMLDivElement = createBalanceElement(
+    account_type_wrapper,
+    debit_cond.category
+  );
+  account_container.appendChild(balance);
+
+  return account_container;
+};
+
+const appendMainContainer = (
+  container: HTMLDivElement,
+  target_category: string
+): void => {
+  const acc: string[] = filterUniqueByCategory(target_category);
+
+  for (let ndx = 0; ndx < acc.length; ndx++) {
+    const debit_obj: data_acc_t = {
+      name: acc[ndx],
+      category: target_category,
+      is_debit: true,
+    };
+
+    const credit_obj: data_acc_t = {
+      name: acc[ndx],
+      category: target_category,
+      is_debit: false,
+    };
+
+    const account_container: HTMLDivElement = createContainerElement(
+      debit_obj,
+      credit_obj
+    );
+
+    container.appendChild(account_container);
+  }
+};
+
+const appendParentContainer = (): void => {
+  const account_category: string[] = [
+    "Asset",
+    "Liability",
+    "Capital",
+    "Revenue",
+    "Expense",
+  ];
+  for (let ndx = 0; ndx < account_category.length; ndx++) {
+    appendMainContainer(account_container_main[ndx], account_category[ndx]);
+  }
+};
+
+appendParentContainer();
