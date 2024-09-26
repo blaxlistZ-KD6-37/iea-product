4f869933a4082e4280d7e8c56fb94d432c3ac1ae
blaxlistZ <blaxlistz@gmail.com>
2024-09-26 11:28:55 +0800

    "Updated functionalities."

--- src/ts/t_balance.ts
+++ src/ts/t_balance.ts
@@ -11,6 +11,34 @@ const financial_accounting_ob = await api.GetDB<financial_accounting_t[]>(
   "financial_account"
 );
 
+// Getting the Trial Balance Body
+const trial_body = <HTMLTableElement>(
+  document.getElementById("trial-balance-body")
+);
+
+// Row Creation
+
+// Table Row
+const account_item: HTMLTableRowElement = document.createElement("tr");
+account_item.classList.add("account-items");
+
+// Table Datas
+const account_balances: HTMLElement[] = [];
+for (let ndx = 0; ndx < 7; ndx++) {
+  account_balances.push(document.createElement("td"));
+}
+account_balances.forEach((bal) => {
+  account_item.appendChild(bal);
+});
+
+const account_vec: string[] = [];
+
+// Unadjusted
+
+// Adjustments
+
+// Adjusted
+
 let category: string[] = financial_accounting_ob.map((acc) => {
   return acc.category;
 });
@@ -19,7 +47,7 @@ category = [...new Set(category)];
 
 const assets: financial_accounting_t[] = financial_accounting_ob.filter(
   (acc) => {
-    return acc.category === category[1];
+    return acc.category === category[0];
   }
 );
 
@@ -31,4 +59,7 @@ let accounts: string[] = assets.map((acc) => {
 
 accounts = [...new Set(accounts)];
 
-console.log(accounts);
+account_vec.push(accounts[0]);
+account_balances[0].textContent = account_vec[0];
+
+trial_body.appendChild(account_item);
