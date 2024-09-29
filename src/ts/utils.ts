909b5d0e4f0eb0b63874d2cd1cb77835ea3195f9
blaxlistZ <blaxlistz@gmail.com>
2024-09-29 13:51:43 +0800

    "Created a utility script."

---
+++ src/ts/utils.ts
@@ -0,0 +1,20 @@
+import "../css/style.css";
+import api from "./api";
+import { financial_accounting_t } from "./database_types";
+
+export const acc_ob = await api.GetDB<financial_accounting_t[]>(
+  "financial_account"
+);
+
+export const FilterDate = (target_month: number): financial_accounting_t[] => {
+  const new_accounting_ob: financial_accounting_t[] = acc_ob.filter((acc) => {
+    const account_month: number = new Date(acc.date).getMonth() + 1;
+    return account_month === target_month;
+  });
+
+  return new_accounting_ob;
+};
+
+export default {
+  FilterDate,
+};
