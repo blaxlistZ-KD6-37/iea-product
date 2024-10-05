398700d2a4cbfa1a1858b9c16f9a4d44fd15b285
blaxlistZ <blaxlistz@gmail.com>
2024-10-05 16:29:04 +0800

    "Finished accounts functionalities."

--- src/ts/accounts.ts
+++ src/ts/accounts.ts
@@ -1,8 +1,11 @@
 import "../css/style.css";
-import api from "./api";
+import api, { GetDB, PostDB } from "./api";
 import { Database_Objects } from "./database_types";
 
-type account_t = Pick<Database_Objects, "name" | "category" | "chart_account">;
+type account_t = Pick<
+  Database_Objects,
+  "account_id" | "name" | "category" | "chart_account"
+>;
 
 const account_ob = await api.GetDB<account_t[]>("account");
 
@@ -14,10 +17,11 @@ const sorted_acc = account_ob
     return acc.chart_account < 998;
   });
 
+// FETCHING ACCOUNTS
 const createChartElement = (account_name: string): HTMLOptionElement => {
   const option_element = <HTMLOptionElement>document.createElement("option");
   option_element.value = account_name;
-  option_element.classList.add("acc-selection", "my-4");
+  option_element.classList.add("acc-selection", "my-4", "p-2");
   option_element.textContent = account_name;
 
   return option_element;
@@ -35,3 +39,108 @@ const appendChartToAccount = () => {
 };
 
 appendChartToAccount();
+
+// SELECTING ACCOUNTS
+type transaction_tab_t = Pick<
+  Database_Objects,
+  "transaction_tab_id" | "date" | "description"
+>;
+
+const tab_ob: transaction_tab_t[] = (
+  await GetDB<transaction_tab_t[]>("transaction")
+).sort((a, b) => {
+  return a.transaction_tab_id - b.transaction_tab_id;
+});
+
+type transaction_detail_t = Pick<
+  Database_Objects,
+  | "transaction_detail_id"
+  | "transaction_tab_id"
+  | "account_id"
+  | "amount"
+  | "is_debit"
+>;
+
+const trans_detail_ob: transaction_detail_t[] = (
+  await GetDB<transaction_detail_t[]>("transaction_detail")
+).sort((a, b) => {
+  return a.transaction_detail_id - b.transaction_detail_id;
+});
+
+const account_post_doc = <HTMLFormElement>(
+  document.querySelector(".account-post")
+);
+
+const debit_child_doc = <HTMLSelectElement>(
+  document.querySelector(".account-deb")
+);
+const credit_child_doc = <HTMLSelectElement>(
+  document.querySelector(".account-cred")
+);
+
+const amount_doc = <HTMLInputElement>document.querySelector(".amt");
+const description_doc = <HTMLInputElement>document.querySelector(".desc");
+
+account_post_doc.addEventListener("submit", async (e: Event) => {
+  e.preventDefault();
+
+  const child_debit = debit_child_doc.value;
+  const child_credit = credit_child_doc.value;
+
+  const latest_tab: number = tab_ob[tab_ob.length - 1].transaction_tab_id + 1;
+  const child_desc: string = description_doc.value;
+
+  const trans_tab: transaction_tab_t = {
+    transaction_tab_id: latest_tab,
+    date: new Date().toISOString(),
+    description: child_desc,
+  };
+
+  await PostDB<transaction_tab_t>("transaction", trans_tab);
+
+  let child_amt: number = parseFloat(amount_doc.value);
+  if (isNaN(child_amt)) {
+    child_amt = 0;
+  }
+
+  const latest_tab_detail: number =
+    trans_detail_ob[trans_detail_ob.length - 1].transaction_detail_id;
+
+  const debit_id: number = (<account_t>sorted_acc.find((acc) => {
+    return acc.name === child_debit;
+  })).account_id;
+
+  type t_tab_t = Pick<
+    Database_Objects,
+    "transaction_tab_id" | "date" | "description"
+  >;
+
+  const getTab = await GetDB<t_tab_t[]>("transaction");
+
+  const newest_tab = getTab[getTab.length - 1].transaction_tab_id;
+
+  const debit_detail: transaction_detail_t = {
+    account_id: debit_id,
+    transaction_tab_id: newest_tab,
+    transaction_detail_id: latest_tab_detail + 1,
+    amount: child_amt,
+    is_debit: true,
+  };
+  await PostDB<transaction_detail_t>("transaction_detail", debit_detail);
+
+  const credit_id: number = (<account_t>sorted_acc.find((acc) => {
+    return acc.name === child_credit;
+  })).account_id;
+
+  const credit_detail: transaction_detail_t = {
+    account_id: credit_id,
+    transaction_tab_id: newest_tab,
+    transaction_detail_id: latest_tab_detail + 2,
+    amount: child_amt,
+    is_debit: false,
+  };
+  await PostDB<transaction_detail_t>("transaction_detail", credit_detail);
+
+  account_post_doc.reset();
+  console.log("Inserted transaction submitted successfully");
+});
