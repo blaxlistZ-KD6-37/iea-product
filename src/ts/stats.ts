c003aa4acccae5671242b0f875408886107e10c6
blaxlistZ <blaxlistz@gmail.com>
2024-10-05 21:39:21 +0800

    "Added chart.js for statistics functionalities."

---
+++ src/ts/stats.ts
@@ -0,0 +1,23 @@
+import { Chart, registerables } from "chart.js";
+import { acc_ob } from "./utils";
+
+Chart.register(...registerables);
+
+const sales = acc_ob.filter((sale) => {
+  return sale.name === "Cash" && sale.is_debit === false;
+});
+
+const chart = <HTMLCanvasElement>document.getElementById("acquisitions");
+
+new Chart(chart, {
+  type: "line",
+  data: {
+    labels: sales.map((row) => new Date(row.date).toUTCString().slice(0, 11)),
+    datasets: [
+      {
+        label: "Acquisitions by month",
+        data: sales.map((row) => row.amount),
+      },
+    ],
+  },
+});
