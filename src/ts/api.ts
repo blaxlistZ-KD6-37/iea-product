1319cb926e5f40fc00fadf1117174c9fe146a319
blaxlistZ <blaxlistz@gmail.com>
2024-10-05 16:26:14 +0800

    "Updated PostDB() and removed FormSubmit()"

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -14,7 +14,10 @@ const requestOptions = (
   };
   if (data_obj !== null) {
     path = {
-      ...path,
+      method: method_type,
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify(data_obj),
     };
   }
@@ -42,21 +45,15 @@ export const GetDB = async <T>(path: string): Promise<T> => {
   }
 };
 
-export const PostDB = async <T extends Record<string, any[]>>(
-  path: string,
-  obj: keyof T,
-  payload: T[keyof T]
-): Promise<void> => {
+export const PostDB = async <T>(path: string, obj: T): Promise<void> => {
   try {
-    const data: T = await GetDB<T>(path);
-    const updated_data = {
-      [obj]: [...(data[obj] || []), ...payload],
-    } as Partial<T>;
-
-    const response = await fetch(
-      `http://localhost:${process.env.PORT}/A1_/runlive/${path}`,
-      requestOptions("POST", new_header, updated_data)
-    );
+    const response = await fetch(`http://localhost:8080/A1_/runlive/${path}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(obj),
+    });
 
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
@@ -67,31 +64,7 @@ export const PostDB = async <T extends Record<string, any[]>>(
   }
 };
 
-export const FormSubmit = <T extends Record<string, any[]>>(
-  path: string,
-  data: keyof T,
-  e: Event
-): void => {
-  e.preventDefault();
-
-  if (!(e.currentTarget instanceof HTMLFormElement)) {
-    console.error("Event target is not a form element!");
-    return;
-  }
-
-  const form: HTMLFormElement = e.currentTarget;
-  const form_data: FormData = new FormData(form);
-
-  const payload: Record<string, FormDataEntryValue> = {};
-  form_data.forEach((content: FormDataEntryValue, ndx: string) => {
-    payload[ndx] = content;
-  });
-
-  PostDB(path, data, <T[keyof T]>[payload]);
-};
-
 export default {
   GetDB,
   PostDB,
-  FormSubmit,
 };
