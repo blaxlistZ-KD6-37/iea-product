828cb647d31ffe846f56f56e17e763cc35fdf04b
blaxlistZ-KD6-37 <blaxlistz@gmail.com>
2024-11-17 16:07:08 +0800

    Created Exchange_Rate_I interface.

--- src/ts/database_types.ts
+++ src/ts/database_types.ts
@@ -12,6 +12,22 @@ export interface Database_Objects {
   is_debit: boolean;
 }
 
+export interface Exchange_Rate_I {
+  result: string;
+  provider: string;
+  documentation: string;
+  terms_of_use: string;
+  time_last_update_unix: number;
+  time_last_update_utc: string;
+  time_next_update_unix: number;
+  time_next_update_utc: string;
+  time_eol_unix: number;
+  base_code: string;
+  rates: {
+    [code: string]: number;
+  };
+}
+
 export type financial_accounting_t = Pick<
   Database_Objects,
   | "name"
