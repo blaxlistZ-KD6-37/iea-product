2afdc6d2a01cab2bfdad590d8a38c86ea1cbc4e3
blaxlistZ <blaxlistz@gmail.com>
2024-11-20 07:12:18 +0800

    "New animations for homepage."

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -6,20 +6,41 @@ const frameworks = <NodeListOf<HTMLElement>>(
 const framework_vec: HTMLElement[] = Array.from(frameworks);
 
 const non_pivot_vec = framework_vec.filter((_, ndx: number): boolean => {
-  return ndx !== 2;
+  return ndx !== 4;
 });
 
 const pivot_vec = <HTMLElement>framework_vec.find((_, ndx: number): boolean => {
-  return ndx === 2;
+  return ndx === 4;
 });
 
+console.log(pivot_vec);
+
+
+
 pivot_vec.addEventListener("click", () => {
+  non_pivot_vec[0].classList.toggle("-translate-x-80");
+  non_pivot_vec[0].classList.toggle("-translate-y-80");
+  non_pivot_vec[0].classList.toggle("row-start-1");
+  non_pivot_vec[0].classList.toggle("col-start-1");
+  non_pivot_vec[0].classList.toggle("z-50");
+
+  non_pivot_vec[1].classList.toggle("translate-x-80");
+  non_pivot_vec[1].classList.toggle("-translate-y-80");
+  non_pivot_vec[1].classList.toggle("z-50");
+
+  non_pivot_vec[2].classList.toggle("-translate-x-80");
+  non_pivot_vec[2].classList.toggle("translate-y-80");
+  non_pivot_vec[2].classList.toggle("z-50");
+
+  non_pivot_vec[3].classList.toggle("translate-x-80");
+  non_pivot_vec[3].classList.toggle("translate-y-80");
+  non_pivot_vec[3].classList.toggle("z-50");
   non_pivot_vec.forEach((frame) => {
     const frame_parent = <HTMLElement>frame.parentElement;
-    frame_parent.classList.toggle("row-start-2");
-    frame_parent.classList.toggle("row-end-2");
-    frame_parent.classList.toggle("col-start-2");
-    frame_parent.classList.toggle("col-end-2");
-    frame_parent.classList.toggle("-z-50");
+    // frame_parent.classList.toggle("row-start-2");
+    // frame_parent.classList.toggle("row-end-2");
+    // frame_parent.classList.toggle("col-start-2");
+    // frame_parent.classList.toggle("col-end-2");
+    frame_parent.classList.toggle("transition-all");
   });
 });
